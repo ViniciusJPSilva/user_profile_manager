77bb20f1f8b3601e02cf5c3838b1c39d20221c2b
Vinícius Pires <viniciusjps15@gmail.com>
2025-07-06 11:54:47 -0300

    Backend created and configured

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const EXPRESS = require("express");
+const APP = EXPRESS();
+const PORT = process.env.PORT || 5000;
+
+
+APP.get('/test', (request, response) => {
+    response.send("<center><h2>Server is online!</h2></center>");
+});
+
+APP.listen(PORT, () => {
+    console.log(`Server is now running on port ${PORT}...`);
+});
